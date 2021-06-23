f87f111f6bd49b7594ec7baec69c5496f3d37a7e
cram <cramarc62@gmail.com>
2021-06-23 17:43:15 +0600

    v 2.1.2

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT=process.env.PORT || 5001;
+const PORT=process.env.PORT || 5000;
 //app.get("/",(req,res)=>{
   //  res.send("hello from server");
 //})
