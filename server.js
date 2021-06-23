5116686b07051905cc042a8ce227828da68d4423
cram <cramarc62@gmail.com>
2021-06-23 18:41:38 +0600

    v2.0.2

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5001;
 //app.get("/",(req,res)=>{
   //  res.send("hello from server");
 //})
